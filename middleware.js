08efd279050192507e4e31e210c2553d82acb8e0
Aditya Raj <heyitsadityaraj@gmail.com>
2026-03-16 17:00:36 +0530

    Optimize middleware size by splitting auth config

--- middleware.js
+++ middleware.js
@@ -1,8 +1,7 @@
-import { auth } from "@/lib/auth";
+import NextAuth from "next-auth";
+import authConfig from "./lib/auth.config";
 
-export default auth((req) => {
-  // Optional: add custom logic here
-});
+export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: [
